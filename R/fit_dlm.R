c25ff3baebe1d923fd176c48e2f7ed8cb4381f45
Michael Bending <michael.a.bending@gmail.com>
2026-04-15 10:08:50 -0400

    fixed some typos/bugs | refactored to add create.dlm()

--- R/fit_dlm.R
+++ R/fit_dlm.R
@@ -1,3 +1,5 @@
+library(dlm)
+
 #' @title create.fitted.model
 #' @description
 #' fit a DLM model on data with trend and seasonal components
@@ -16,29 +18,37 @@
 create.fitted.model <- function(data,
                                 trend.order = NULL,
                                 seasonal.periods= NULL) {
-  
-    build.fn <- function(param) {
+    create.dlm <- function() {
       if (is.null(trend.order) || trend.order == 0) {
         mod = NULL
+      } else {
+        mod = dlmModPoly(order=trend.order)
+      }
+      for (s in seasonal.periods) {
+        if (is.null(mod)) {
+          mod = dlmModSeas(s)
+        } else {
+          mod = mod + dlmModSeas(s)
+        }
+      }
+      mod
+    }
+    
+    build.fn <- function(param) {
+      if (is.null(trend.order) || trend.order == 0) {
         W.diag = c()
         p.index = 1
       } else {
-        mod = dlmModPoly(order = trend.order)
         W.diag = c(exp(param[2:(1+trend.order)]))
         p.index = 2+trend.order
       }
       
       for (s in seasonal.periods) {
-        if (is.null(mod)) {
-          mod = dlmModSeas(frequency=s)
-        } else {
-          mod = mod + dlmModSeas(frequency=s)
-        }
-        
         W.diag = c(W.diag, exp(param[p.index]), rep(0, s-2))
         p.index = p.index + 1
       }
       
+      mod = create.dlm()
       V(mod) <- exp(param[1])
       diag(W(mod)) <- W.diag
       return(mod)
@@ -51,21 +61,7 @@ create.fitted.model <- function(data,
     n.coef = trend.order.number + sum(seasonal.periods) - length(seasonal.periods) - 1
     fit <- dlmMLE(data, rep(0, n.coef), build=build.fn, hessian=TRUE)
     
-    if (trend.order.number == 0) {
-      mod = NULL
-    } else {
-      mod = dlmModPoly(trend.order.number)
-    }
-    
-    for (s in seasonal.periods) {
-      if (is.null(mod)) {
-        mod = dlmModSeas(s)
-      } else {
-        mod = mod + dlmModSeas(s)
-      }
-    }
-    
-    loglik <- dlmLL(data, dlmModPoly(1) + dlmModSeas(4))
+    loglik <- dlmLL(data, create.dlm())
     fit.mod <- build.fn(fit$par)
     
     filtered <- dlmFilter(data, mod=fit.mod)
@@ -77,8 +73,8 @@ create.fitted.model <- function(data,
       trend.order = trend.order,
       seasonal.periods = seasonal.periods,
       mod = fit.mod,
-      obs.error.var = V(mod),
-      state.error.var = diag(W(mod)),
+      obs.error.var = V(fit.mod),
+      state.error.var = diag(W(fit.mod)),
       
       n.coef = n.coef,
       loglik = loglik,
