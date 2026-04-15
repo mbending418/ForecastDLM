c5e14ca1aad6d7d57eb74f8fc5c280b5db066fc9
Michael Bending <michael.a.bending@gmail.com>
2026-04-15 10:49:41 -0400

    fixed typo

--- R/fit_dlm.R
+++ R/fit_dlm.R
@@ -53,7 +53,7 @@ create.fitted.model <- function(data,
       diag(W(mod)) <- W.diag
       return(mod)
     }
-    if (is.null(trend.order)) {
+    if (is.null(trqend.order)) {
       trend.order.number = 0
     } else {
       trend.order.number = trend.order
